f9984318cf01cb052bfeecdb45383d69925056f5
gzz <gzz@163.com>
2020-04-03 09:35:29 +0800

    加个网站

--- src/main/java/com/gzz/Application25.java
+++ src/main/java/com/gzz/Application25.java
@@ -1,35 +1,32 @@
 package com.gzz;
-
 import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
 public class Application25 {
-	private static Log logger = LogFactory.getLog(Application25.class);
 	private static String root = "E:/aaaa/";
 	private static DecimalFormat df = new DecimalFormat("00");
-
 	public static void main(String[] args) throws IOException {
-		for (int i = 1; i < 26; i++) {
+		for (int i = 16; i < 26; i++) {
 			Document document = Jsoup.connect("http://www.tuyim.net/forum-35-" + i + ".html").get();
 			Elements elements = document.select("div.bus_vtem a.preview");
 			for (Element element : elements) {
-				if (count(element.attr("title")) < 369) {
+				String title = element.attr("title")
+						.replace("MyGirl美媛馆 ", "").replace("[MyGirl美媛馆] ", "")
+						.replace("[MyGirl美媛馆]", "").replaceAll("(?i)(vol)", "VOL");
+				if (count(title) < 160) {
 					String base = element.attr("href");
-					logger.info(element.attr("title"));
+					log.info("i={},{}", i, title);
 					Elements eles = Jsoup.connect(base).get().select(".mbn.savephotop img");
-//				logger.info(eles);
 					int num = 1;
 					for (Element ele : eles) {
-						String path = root + element.attr("title") + "/" + df.format(num) + ".jpg";
+						String path = root + title + "/" + df.format(num) + ".jpg";
 						Utils.downPic(ele.attr("data-original"), path, base);
 						num++;
 					}
@@ -37,15 +34,11 @@ public class Application25 {
 			}
 		}
 	}
-
 	public static int count(String title) {
 		Matcher matcher = Pattern.compile("(.*(?i)OL.)(\\d{3})(\\s+.*)").matcher(title);
-		// 因此可以改写为
 		if (matcher.matches()) {
-//			logger.info("[num=" + matcher.group(2));
 			return new Integer(matcher.group(2));
 		}
-		return -1;
+		return 357;
 	}
-
 }
---
+++ src/main/java/com/gzz/Application26.java
@@ -0,0 +1,49 @@
+package com.gzz;
+
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
+public class Application26 {
+ 
+	private static String root = "E:/a111/";
+	private static DecimalFormat df = new DecimalFormat("00");
+
+	public static void main(String[] args) throws IOException {
+		for (int i = 6; i < 12; i++) {
+			Document document = Jsoup.connect("http://www.tuyim.net/forum-341-" + i + ".html").get();
+			Elements elements = document.select("div.bus_vtem a.preview");
+			for (Element element : elements) {
+				String title = element.attr("title");
+				if (count(title) <= 120) {
+					String base = element.attr("href");
+					log.info("i={},{}",i,title);
+					Elements eles = Jsoup.connect(base).get().select(".mbn.savephotop img");
+					int num = 1;
+					for (Element ele : eles) {
+						String path = root + title + "/" + df.format(num) + ".jpg";
+						Utils.downPic(ele.attr("data-original"), path, base);
+						num++;
+					}
+				}
+			}
+		}
+	}
+
+	public static int count(String title) {
+		Matcher matcher = Pattern.compile("(.*(?i)OL.)(\\d{3})(\\s+.*)").matcher(title);
+		if (matcher.matches()) {
+			return new Integer(matcher.group(2));
+		}
+		return 357;
+	}
+
+}
